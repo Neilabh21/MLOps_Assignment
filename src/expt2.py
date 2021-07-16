8ce831c1afb94fdb6152c30c36d1b816ea4c0944
Neilabh Banzal <neilabhbanzal21@gmail.com>
2021-07-16 20:45:23 +0530

    Update expt2.py

--- src/expt2.py
+++ src/expt2.py
@@ -5,11 +5,8 @@ import dvc.api
 
 import pickle
 
-with dvc.api.open(
-	'data/creditcard.csv',
-	repo = "https://github.com/Neilabh21/MLOps_Assignment"
-	) as fd:
-	data = pd.read_csv(fd)
+with dvc.api.open(repo="https://github.com/Neilabh21/MLOps_Assignment", path="data/creditcard.csv", mode="r") as fd:
+		data = pd.read_csv(fd)
 
 X = data.drop(['Class'], axis=1)
 Y = data["Class"]
@@ -22,8 +19,8 @@ from sklearn.model_selection import train_test_split
 # Split the data into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size = 0.2, random_state = 42)
 
-X_train.to_csv("../data/processed/train.csv", index=False)
-X_test.to_csv("../data/processed/test.csv", index=False)
+pd.DataFrame(X_train).to_csv("../data/processed/train.csv", index=False)
+pd.DataFrame(X_test).to_csv("../data/processed/test.csv", index=False)
 
 # Import Decision Tree Classifier
 from sklearn.ensemble import RandomForestClassifier
@@ -32,7 +29,7 @@ from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier()
 
 # Train Decision Tree Classifer
-clf = clf.fit(X_train,y_train)
+clf = clf.fit(X_train,Y_train)
 
 #Predict the response for test dataset
 y_pred = clf.predict(X_test)
@@ -42,9 +39,8 @@ pickle.dump(clf, open("../models/model.pkl", 'wb'))
 
 from sklearn import metrics
 import json
-with open('../metrics/acc_f1.json', 'w') as f:
-    json.dump(metrics.accuracy_score(y_test, y_pred), f)
-    json.dump(metrics.f1_score(y_test, y_pred), f)
+with open('acc_f1.json', 'w') as f:
+    json.dump([metrics.accuracy_score(Y_test, y_pred), metrics.f1_score(Y_test, y_pred)], f)
 
 
 
